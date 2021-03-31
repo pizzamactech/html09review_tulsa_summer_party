2279f7653e35c711bc4ae75e9fd9738bee9dd641
pizzamactech <81186887+pizzamactech@users.noreply.github.com>
2021-03-31 12:40:11 -0400

    Update tny_timer.js

--- tny_timer.js
+++ tny_timer.js
@@ -9,10 +9,11 @@
    Date:  3/30/2021 
 
 */
-/* Function to create and show the countdown clock */
 showClock();
 setInterval("showClock()",1000);
 
+/* Function to create and show the countdown clock */
+function showClock() {
 /* Store the current date and time */
 var thisDay = new Date("May 19, 2018 9:31:27");
 var localDate = thisDay.toLocaleDateString();
@@ -20,14 +21,7 @@ var localTime = thisDay.toLocaleTimeString();
 
 /* Display the current date and time */
 document.getElementById("currentTime").innerHTML =
-   "<span>" + localDate + "</span><span>" + localDate + "</span">
-function nextJuly4(currentDate) {
-   var cYear = currentDate.getFullYear();
-   var jDate = new Date("July 4, 2018");
-   jDate.setFullYear(cYear);
-   if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
-   return jDate;
-}
+"<span>" + localDate + "</span><span>" + localDate + "</span">   
 
 function nextJuly4(thisDay) {
  j4Date.sethours(21);
@@ -37,9 +31,21 @@ function nextJuly4(thisDay) {
   var hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
   var minsLeft = (hrsleft - Math.floor(hrsLeft))*60;
   var secsLeft = (minsLeft - Math.floor(minsLeft))*60;
- /* Display the time left until July 4th */
+ 
+/* Display the time left until July 4th */
   document.getElementById("days").textContent = Math.floor(daysLeft);
   document.getElementById("hrs").textContent = Math.floor(hrsLeft);
   document.getElementById("mins").textContent = Math.floor(minsLeft);
   document.getElementById("secs").textContent = Math.floor(secsLeft);
+}
+function nextJuly4(currentDate) {
+   var cYear = currentDate.getFullYear();
+   var jDate = new Date("July 4, 2018");
+   jDate.setFullYear(cYear);
+   if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
+   return jDate;
+}
+
+
+
 
